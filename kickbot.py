361a60f156d1cee25a1c294ffb0576b8b306294b
Togn3K <togne193@gmail.com>
2017-08-08 17:50:00 +0200

    added command for append words and function that write in csv file. NOT WORKING

--- kickbot.py
+++ kickbot.py
@@ -9,9 +9,31 @@ forbiddenWords = []
 def importCSV():
 	with open('forbiddenWords.csv','rb') as f:
 		reader = csv.reader(f)
-		for row in reader: 
+		for row in reader:
 			forbiddenWords.extend(row)
 		print(forbiddenWords)
+	f.close()
+
+def appendCSV(word):
+	word.lower()
+	# check if word is barely legal
+	for fw in forbiddenWords:
+		if fw == word:
+			return False
+	f=open('forbiddenWords.csv','wb')
+	writer=csv.writer(f,delimiter='',quotechar="",quoting=csv.QUOTE_ALL)
+	writer.writerow(word)
+	f.close()
+	forbiddenWords.extend(word)
+	print("Word %s added!",word)
+	return True
+
+def snforbidden(bot,update):
+	update.message.reply_text("Uhuh! Delizioso!")
+	sendedText=update.message.text.split()
+	if len(sendedText) > 1:
+		for w in sendedText[1:]:
+			appendCSV(w)
 
 def start(bot,update):
 	update.message.reply_text("Il gioco è già iniziato.")
@@ -22,7 +44,7 @@ def help(bot,update):
 def function(bot,update):
 	# update.message.reply_text(update.message.text)
 	# print(update.message.text) 			# take text message
-	# print(update.message.chat_id) 		# take chat id 
+	# print(update.message.chat_id) 		# take chat id
 	# print(update.message.from_user.id) 		# take user id
 	user_message = update.message.text.lower()
 	for word in forbiddenWords:
@@ -40,12 +62,13 @@ def main():
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start",start))
 	dp.add_handler(CommandHandler("help",help))
+	dp.add_handler(CommandHandler("snforbidden",snforbidden))
 	dp.add_handler(MessageHandler(Filters.text,function))
 	updater.start_polling()
 	try:
 		print("Ready")
 		updater.idle()
-	except KeyboardInterrupt: 
+	except KeyboardInterrupt:
 		print("questo print non funziona")
 
 if __name__ == '__main__':
